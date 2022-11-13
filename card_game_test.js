c7e26a27b97e81a513fd31735e67d7124b3ab116
Judith Espinoza <47121765+jespinoza2019@users.noreply.github.com>
2022-11-13 00:40:15 -0700

    Add files via upload

---
+++ card_game_test.js
@@ -0,0 +1,9 @@
+
+describe ('My comparing player card',function(){
+    describe ('#compare', function(){
+        it ('it should return a true if first number is the first array  greater first number of the second array )', function(){
+            var x = compare(5,4);
+            expect(x).to.greaterThan(y)
+         })
+     })
+})
